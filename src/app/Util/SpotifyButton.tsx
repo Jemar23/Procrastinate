dffc9971f0ec08d6f245a886161899dce2bd3f2e
Jemar23 <jemarjohnson116@icloud.com>
2023-06-07 22:20:27 -0400

    add button functionality

--- src/app/Util/SpotifyButton.tsx
+++ src/app/Util/SpotifyButton.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { SpotifyIcon } from '../Util/Icon'
 
 export default function SpotifyButton() {
     return(
-        <div className="flex items-center justify-center text-center m-8 p-8 w-24 bg-transparent backdrop-blur-2xl dark:border-green-800 rounded-md border border-green-600 shadow-lg hover:shadow-green-700 overflow-hidden">
+        <button className="flex items-center justify-center text-center m-8 p-8 w-24 bg-transparent backdrop-blur-2xl dark:border-green-800 rounded-md border border-green-600 shadow-lg hover:shadow-green-700 overflow-hidden">
             <SpotifyIcon />
-        </div>
+        </button>
     );
 }
--- src/app/Util/YoutubeButton.tsx
+++ src/app/Util/YoutubeButton.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { YoutubeIcon } from '../Util/Icon'
 
 export default function YoutubeButton() {
     return(
-        <div className="flex items-center justify-center text-center m-8 p-8 w-24 bg-transparent backdrop-blur-2xl dark:border-red-800 rounded-md border border-slate-600 shadow-lg hover:shadow-red-600 overflow-hidden">
+        <button className="flex items-center justify-center text-center m-8 p-8 w-24 bg-transparent backdrop-blur-2xl dark:border-red-800 rounded-md border border-slate-600 shadow-lg hover:shadow-red-600 overflow-hidden">
             <YoutubeIcon />
-        </div>
+        </button>
     );
 }
